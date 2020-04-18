33d57cce3341454fbac9047171e6d88ec61ae2d5
Jan Blumenkamp <jb2270@cam.ac.uk>
2020-04-18 13:14:57 +0200

    Reimplemented PID controller hierarchy
    Adds a low-level rate controller that translates angular velocites to motor commands instead directly translating desired angles to motor commands, which makes everything much more stable

--- copters/quadcopter.py
+++ copters/quadcopter.py
@@ -6,28 +6,34 @@ DEFAULT_CONFIG = {
     'step_freq': 240,
     'reset_pos': [0, 0, 1],
     'pid': {
-        'roll': [0.2, 0.01, 0.1],
-        'pitch': [0.2, 0.01, 0.1],
-        'yaw': [1, 0, 0],
-        'vx': [0.5, 0.2, 0.01],
-        'vy': [0.5, 0.2, 0.01],
+        'roll_rate': [1, 0.1, 0.001],
+        'pitch_rate': [1, 0.1, 0.001],
+        'yaw_rate': [1, 0, 0],
+        'roll_angle': [10, 0, 0],
+        'pitch_angle': [10, 0, 0],
+        'vx': [0.3, 0.01, 0.001],
+        'vy': [0.3, 0.01, 0.001],
         'vz': [100, 1, 0.1]
     },
     'limits': {
         'thrust': (0, 6),
-        'pitch': (-np.pi/8, np.pi/8),
-        'roll': (-np.pi/8, np.pi/8),
-        'yaw': (-2*np.pi, 2*np.pi),
-        'pid_pitch_out': (-3, 3),
-        'pid_roll_out': (-3, 3),
-        'pid_yaw_out': (-3, 3),
+        'pitch_angle': (-np.pi/8, np.pi/8),
+        'roll_angle': (-np.pi/8, np.pi/8),
+        'pitch_rate': (-np.pi/2, np.pi/2),
+        'roll_rate': (-np.pi/2, np.pi/2),
+        'yaw_rate': (-2*np.pi, 2*np.pi),
+        'pid_pitch_rate_out': (-3, 3),
+        'pid_roll_rate_out': (-3, 3),
+        'pid_yaw_rate_out': (-3, 3),
         'motor_force': (0, 15),
         'yaw_torque': (-0.5, 0.5),
         'vx': (-12, 12),
         'vy': (-12, 12),
-        'vz': (-7, 1.5), # if we allow higher rising speeds, there are some issues with the speed controllers
+        'vz': (-7, 2), # if we allow higher rising speeds, there are some issues with the speed controllers
     },
     'wind_vector': [0.2, 0.1, 0.03],
+    #'wind_vector': [0, 0, 0],
+    'force_motor_noise': 2,
     'force_noise_fac': 2,
     'torque_noise_fac': 0.1
 }
@@ -36,6 +42,7 @@ class Quadcopter():
     def __init__(self, pybullet_client, cfg=DEFAULT_CONFIG):
         self.pybullet_client = pybullet_client
         self.cfg = cfg
+        self.cfg['dt'] = 1/self.cfg['step_freq']
         self.initial_pos = self.position = np.array(self.cfg['reset_pos'])
         import os
         p.setAdditionalSearchPath(os.path.dirname(os.path.abspath(__file__)))
@@ -44,12 +51,16 @@ class Quadcopter():
         self.simulated_wind_vector = np.array(self.cfg['wind_vector'])
 
         self.thrust = 0
-        self.setpoint_roll = 0
-        self.setpoint_pitch = 0
-        self.setpoint_yaw = 0
-        self.roll_pid = PID(*self.cfg['pid']['roll'])
-        self.pitch_pid = PID(*self.cfg['pid']['pitch'])
-        self.yaw_pid = PID(*self.cfg['pid']['yaw'])
+        self.setpoint_roll_rate = 0
+        self.setpoint_pitch_rate = 0
+        self.setpoint_yaw_rate = 0
+        self.setpoint_roll_angle = 0
+        self.setpoint_pitch_angle = 0
+        self.pid_roll_rate = PID(*self.cfg['pid']['roll_rate'])
+        self.pid_pitch_rate = PID(*self.cfg['pid']['pitch_rate'])
+        self.pid_yaw_rate = PID(*self.cfg['pid']['yaw_rate'])
+        self.pid_roll_angle = PID(*self.cfg['pid']['roll_angle'])
+        self.pid_pitch_angle = PID(*self.cfg['pid']['pitch_angle'])
 
         self.pid_vx = PID(*self.cfg['pid']['vx'])
         self.pid_vy = PID(*self.cfg['pid']['vy'])
@@ -60,17 +71,15 @@ class Quadcopter():
     def reset(self):
         p.resetBasePositionAndOrientation(self.body_id, self.initial_pos, p.getQuaternionFromEuler([0,0,0*(np.pi/180)]), physicsClientId=self.pybullet_client)
         self.update_state()
-        self.last_orientation = self.orientation
-        self.last_position = self.position
 
-        self.roll_pid.reset()
-        self.pitch_pid.reset()
-        self.yaw_pid.reset()
+        self.pid_roll_rate.reset()
+        self.pid_pitch_rate.reset()
+        self.pid_yaw_rate.reset()
+        self.pid_roll_angle.reset()
+        self.pid_pitch_angle.reset()
         self.pid_vx.reset()
         self.pid_vy.reset()
         self.pid_vz.reset()
-
-        self.compute_speed()
 
     def update_state(self):
         position, orientation = p.getBasePositionAndOrientation(self.body_id, physicsClientId=self.pybullet_client)
@@ -78,44 +87,46 @@ class Quadcopter():
         self.orientation = np.array(orientation)
         self.orientation_euler = np.array(p.getEulerFromQuaternion(orientation))
 
-    def get_rotation_matrix(self):
-        return np.array(p.getMatrixFromQuaternion(self.orientation)).reshape(3,3)
-
-    def compute_speed(self):
         m = self.get_rotation_matrix()
-        self.absolute_speed = (self.position - self.last_position)*self.cfg['step_freq']
-        self.lateral_speed = self.absolute_speed @ m
-        delta_speed_quat = p.getDifferenceQuaternion(self.last_orientation, self.orientation)
-        self.ang_speed = np.array(p.getEulerFromQuaternion(delta_speed_quat))*self.cfg['step_freq']
-        self.last_orientation = self.orientation
-        self.last_position = self.position
+        lin_vel, ang_vel = p.getBaseVelocity(self.body_id, self.pybullet_client)
+        self.world_lateral_speed = np.array(lin_vel)
+        self.lateral_speed = self.world_lateral_speed @ m
+        self.world_ang_speed = np.array(ang_vel)
+        self.ang_speed = self.world_ang_speed @ m
+
+    def get_rotation_matrix(self, orientation=None):
+        return np.array(p.getMatrixFromQuaternion(self.orientation if orientation is None else orientation)).reshape(3,3)
 
     def set_thrust(self, thrust):
         self.thrust = np.clip(thrust, *self.cfg['limits']['thrust'])
 
-    def set_pitch(self, pitch):
-        self.setpoint_pitch = np.clip(pitch, *self.cfg['limits']['pitch'])
+    def set_pitch_rate(self, pitch):
+        self.setpoint_pitch_rate = np.clip(pitch, *self.cfg['limits']['pitch_rate'])
 
-    def set_roll(self, roll):
-        self.setpoint_roll = np.clip(roll, *self.cfg['limits']['roll'])
+    def set_roll_rate(self, roll):
+        self.setpoint_roll_rate = np.clip(roll, *self.cfg['limits']['roll_rate'])
 
-    def set_yaw(self, yaw):
-        self.setpoint_yaw = np.clip(yaw, *self.cfg['limits']['yaw'])
+    def set_pitch_angle(self, pitch):
+        self.setpoint_pitch_angle = np.clip(pitch, *self.cfg['limits']['pitch_angle'])
+
+    def set_roll_angle(self, roll):
+        self.setpoint_roll_angle = np.clip(roll, *self.cfg['limits']['roll_angle'])
+
+    def set_yaw_rate(self, yaw):
+        self.setpoint_yaw_rate = np.clip(yaw, *self.cfg['limits']['yaw_rate'])
 
     def step(self):
         self.update_state()
-        self.compute_speed()
 
-        dt = 1/self.cfg['step_freq']
-        ctrl_pitch = np.clip(self.pitch_pid.step(self.setpoint_pitch, self.orientation_euler[1], dt), *self.cfg['limits']['pid_pitch_out'])
-        ctrl_roll = np.clip(self.roll_pid.step(self.setpoint_roll, self.orientation_euler[0], dt), *self.cfg['limits']['pid_roll_out'])
-        ctrl_yaw = np.clip(self.yaw_pid.step(self.setpoint_yaw, self.ang_speed[2], dt), *self.cfg['limits']['pid_yaw_out'])
+        ctrl_pitch = np.clip(self.pid_pitch_rate.step(self.setpoint_pitch_rate, self.ang_speed[1], self.cfg['dt']), *self.cfg['limits']['pid_pitch_rate_out'])
+        ctrl_roll = np.clip(self.pid_roll_rate.step(self.setpoint_roll_rate, self.ang_speed[0], self.cfg['dt']), *self.cfg['limits']['pid_roll_rate_out'])
+        ctrl_yaw = np.clip(self.pid_yaw_rate.step(self.setpoint_yaw_rate, self.ang_speed[2], self.cfg['dt']), *self.cfg['limits']['pid_yaw_rate_out'])
         translation_forces = np.clip(np.array([
             self.thrust + ctrl_roll - ctrl_pitch,
             self.thrust - ctrl_roll - ctrl_pitch,
             self.thrust + ctrl_roll + ctrl_pitch,
             self.thrust - ctrl_roll + ctrl_pitch,
-        ]), *self.cfg['limits']['motor_force'])
+        ]) + np.random.uniform(-self.cfg['force_motor_noise'], self.cfg['force_motor_noise'], 4), *self.cfg['limits']['motor_force'])
         yaw_torque = np.clip(ctrl_yaw, *self.cfg['limits']['yaw_torque'])
 
         motor_points = np.array([[1,1,0], [1,-1,0], [-1,1,0], [-1,-1,0]])*0.2
@@ -128,9 +139,12 @@ class Quadcopter():
         p.applyExternalTorque(self.body_id, 0, (np.random.rand(3)-0.5)*self.cfg['torque_noise_fac'], p.LINK_FRAME, physicsClientId=self.pybullet_client)
         p.applyExternalForce(self.body_id, -1, self.simulated_wind_vector, self.position, p.WORLD_FRAME, physicsClientId=self.pybullet_client)
 
+    def step_angle(self):
+        self.set_roll_rate(self.pid_roll_angle.step(self.setpoint_roll_angle, self.orientation_euler[0], self.cfg['dt']))
+        self.set_pitch_rate(self.pid_pitch_angle.step(self.setpoint_pitch_angle, self.orientation_euler[1], self.cfg['dt']))
+
     def step_speed(self, vx, vy, vz):
-        dt = 1/self.cfg['step_freq']
-        self.set_pitch(self.pid_vx.step(np.clip(vx, *self.cfg['limits']['vx']), self.lateral_speed[0], dt))
-        self.set_roll(self.pid_vy.step(np.clip(vy, *self.cfg['limits']['vy']), -self.lateral_speed[1], dt))
-        self.set_thrust(self.pid_vz.step(np.clip(vz, *self.cfg['limits']['vz']), self.lateral_speed[2], dt))
+        self.set_pitch_angle(self.pid_vx.step(np.clip(vx, *self.cfg['limits']['vx']), self.lateral_speed[0], self.cfg['dt']))
+        self.set_roll_angle(self.pid_vy.step(np.clip(vy, *self.cfg['limits']['vy']), -self.lateral_speed[1], self.cfg['dt']))
+        self.set_thrust(self.pid_vz.step(np.clip(vz, *self.cfg['limits']['vz']), self.lateral_speed[2], self.cfg['dt']))
 
--- test_drone.py
+++ test_drone.py
@@ -42,7 +42,7 @@ def print_info():
 
 #threading.Thread(target=print_info, args=()).start()
 
-def eval_pid_roll():
+def eval_pid_rate_roll(client):
     pif_h = PID(2, 2.4, 0.84)
     desired_height = 3
     angle_sign = -1
@@ -51,25 +51,105 @@ def eval_pid_roll():
     debug = []
     timestep = []
     current_time = 0
-    start_logging = False
+    state = 0
+    setpoint = 0
+    state_change_time = 0
     while True:
-        drone.set_pitch(0)
-        setpoint = 0
-        if start_logging:
-            setpoint = np.pi/8*angle_sign
-        setpoints.append(setpoint)
-        real_data.append(drone.orientation_euler[0])
-        debug.append(drone.roll_pid.integral)
-        timestep.append(current_time)
-        current_time += 1/240
-        drone.set_roll(setpoint)
-        drone.set_yaw(0)
+        if state == 0:
+            if current_time > 1:
+                state = 1
+        elif state == 1:
+            setpoint = np.pi/2
+            if drone.orientation_euler[0] > np.pi/7:
+                state_change_time = current_time
+                setpoint = 0
+                state = 2
+        elif state == 2:
+            if current_time - state_change_time > 2:
+                setpoint = -np.pi/4
+                state = 3
+        elif state == 3:
+            if drone.orientation_euler[0] < -np.pi/7:
+                setpoint = 0
+                state_change_time = current_time
+                state += 1
+        elif state == 4:
+            if current_time - state_change_time > 2:
+                break
+
+        drone.set_roll_rate(setpoint)
+        drone.set_pitch_rate(0)
+        drone.set_yaw_rate(0)
         drone.set_thrust(pif_h.step(desired_height, drone.position[2], 1/240))
         drone.step()
 
+        setpoints.append(setpoint)
+        real_data.append(drone.ang_speed[0])
+        #debug.append(drone.roll_pid.error)
+        timestep.append(current_time)
+        current_time += 1/240
+
+        if np.linalg.norm(drone.position, ord=2) > 100:
+            drone.reset()
+
+        p.stepSimulation(physicsClientId=client)
+        p.resetDebugVisualizerCamera(5, 50, -35, drone.position, client)
+        #time.sleep(1/240)
+
+    plt.plot(timestep, setpoints, label="setpoint")
+    plt.plot(timestep, real_data, label="real")
+    #plt.plot(timestep, debug, label="debug")
+    #plt.ylim(-0.3,1)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def eval_pid_roll(client):
+    pif_h = PID(2, 2.4, 0.84)
+    desired_height = 3
+    angle_sign = -1
+    setpoints = []
+    real_data = []
+    debug = []
+    timestep = []
+    current_time = 0
+    state = 0
+    setpoint = 0
+    while True:
+        if state == 0:
+            if drone.position[2] >= desired_height:
+                setpoint = np.pi/8
+                state += 1
+        elif state == 1:
+            if drone.position[1] < -10:
+                state += 1
+                print("N")
+                setpoint = -np.pi/16
+        elif state == 2:
+            if drone.position[1] > 0:
+                setpoint = -np.pi/32
+                state += 1
+        elif state == 3:
+            if drone.position[1] > 10:
+                break
+
+        drone.set_pitch_angle(0)
+        drone.set_roll_angle(setpoint)
+        drone.set_yaw_rate(0)
+        drone.set_thrust(pif_h.step(desired_height, drone.position[2], 1/240))
+        drone.step_angle()
+        drone.step()
+
+        setpoints.append(setpoint)
+        real_data.append(drone.orientation_euler[0])
+        #debug.append(drone.roll_pid.error)
+        timestep.append(current_time)
+        current_time += 1/240
+
+        '''
         if drone.position[2] >= desired_height:
             start_logging = True
-        if drone.position[1] > 6:
+        if drone.position[1] > 80:
             angle_sign = 1
         elif drone.position[1] < -10:
             break
@@ -77,13 +157,52 @@ def eval_pid_roll():
             angle_sign = 0.1
         elif drone.position[1] < -1:
             angle_sign = 0.5
-        
-        if np.linalg.norm(drone.position, ord=2) > 15:
+        '''
+        if np.linalg.norm(drone.position, ord=2) > 100:
             drone.reset()
-    
+
+        p.resetDebugVisualizerCamera(5, 50, -35, drone.position, client)
         p.stepSimulation(physicsClientId=client)
         #time.sleep(1/240)
-    
+
+    plt.plot(timestep, setpoints, label="setpoint")
+    plt.plot(timestep, real_data, label="real")
+    #plt.plot(timestep, debug, label="debug")
+    #plt.ylim(-0.3,1)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def test_pid_roll(client):
+    pif_h = PID(2, 2.4, 0.84)
+    desired_height = 3
+    current_time = 0
+    setpoints = []
+    real_data = []
+    debug = []
+    timestep = []
+    while True:
+        drone.set_pitch_angle(0)
+        current_time += 1/240
+        setpoint = np.random.uniform(-np.pi/4, np.pi/4)
+        setpoints.append(setpoint)
+        real_data.append(drone.orientation_euler[0])
+        timestep.append(current_time)
+        debug.append(drone.pid_roll_rate.integral)
+        drone.set_roll_angle(setpoint)
+        drone.set_yaw_rate(0)
+        drone.set_thrust(pif_h.step(desired_height, drone.position[2], 1/240))
+        drone.step_angle()
+        drone.step()
+
+        if np.linalg.norm(drone.position, ord=2) > 15:
+            drone.reset()
+        if current_time > 10:
+            break
+
+        p.stepSimulation(physicsClientId=client)
+        #time.sleep(1/240)
+
     plt.plot(timestep, setpoints)
     plt.plot(timestep, real_data)
     plt.plot(timestep, debug)
@@ -91,49 +210,58 @@ def eval_pid_roll():
     plt.grid()
     plt.show()
 
-def eval_pid_yaw():
+def eval_pid_yaw(client):
     pif_h = PID(4, 2.4, 0.42)
     desired_height = 2
     angle_sign = 1
     setpoints = []
+    debug = []
     real_data = []
     timestep = []
     current_time = 0
-    start_logging = False
+    state_change_time = 0
+    state = 0
+    setpoint = 0
     while True:
-        drone.set_pitch(0)
-        setpoint = 0
-        if start_logging:
-            setpoint = 2*np.pi*angle_sign
+        if state == 0:
+            if current_time > 0.1:
+                setpoint = 2*np.pi
+                state += 1
+        elif state == 1:
+            if drone.orientation_euler[2] > np.pi/2:
+                setpoint = -np.pi
+                state += 1
+        elif state == 2:
+            if drone.orientation[2] < -np.pi/4:
+                break
+
+        drone.set_pitch_rate(0)
+        drone.set_roll_rate(0)
+        drone.set_yaw_rate(setpoint)
+        drone.set_thrust(pif_h.step(desired_height, drone.position[2], 1/240))
+        drone.step()
+
         setpoints.append(setpoint)
         real_data.append(drone.ang_speed[2])
+        debug.append(drone.pid_yaw_rate.error)
         timestep.append(current_time)
         current_time += 1/240
-        drone.set_roll(0)
-        drone.set_yaw(setpoint)
-        drone.set_thrust(pif_h.step(desired_height, drone.position[2], 1/240))
-        if current_time > 0.1:
-            start_logging = True
-        if angle_sign == 1 and drone.orientation[2] > np.pi/4:
-            angle_sign = -1
-        elif angle_sign == -1 and drone.orientation[2] < -np.pi/4:
-            break
-        
-        drone.step()
-        
+
         if np.linalg.norm(drone.position, ord=2) > 15:
             drone.reset()
-    
+
+        p.resetDebugVisualizerCamera(5, 50, -35, drone.position, client)
         p.stepSimulation(physicsClientId=client)
         time.sleep(1/240)
-    
+
     plt.plot(timestep, setpoints)
     plt.plot(timestep, real_data)
+    plt.plot(timestep, debug)
     #plt.ylim(-0.3,1)
     plt.grid()
     plt.show()
 
-def remote_control():
+def remote_control(client):
     keyboard.add_hotkey('up', keyboard_command, args=('pitch', -np.pi/16))
     keyboard.add_hotkey('down', keyboard_command, args=('pitch', np.pi/16))
     keyboard.add_hotkey('left', keyboard_command, args=('roll', np.pi/16))
@@ -143,15 +271,15 @@ def remote_control():
     keyboard.add_hotkey('ctrl', keyboard_command, args=('reset', 0))
     keyboard.add_hotkey(',', keyboard_command, args=('yaw', -0.1))
     keyboard.add_hotkey('.', keyboard_command, args=('yaw', 0.1))
-    
+
     pid_h = PID(3, 0.24, 0.625)
     desired_height = 2
     while True:
-        print(drone.lateral_speed)
         drone.step_speed(0, 0, pid_h.step(desired_height, drone.position[2], 1/240))
-        drone.set_pitch(current_control['pitch'])
-        drone.set_roll(current_control['roll'])
-        drone.set_yaw(current_control['yaw'])
+        drone.set_pitch_angle(current_control['pitch'])
+        drone.set_roll_angle(current_control['roll'])
+        drone.set_yaw_rate(current_control['yaw'])
+        drone.step_angle()
         drone.step()
         if np.linalg.norm(drone.position, ord=2) > 10:
             drone.reset()
@@ -185,16 +313,17 @@ def eval_pid_speed_hor(client):
         real_data.append(drone.lateral_speed[0])
         timestep.append(current_time)
         current_time += 1/240
-        
-        drone.step_speed(setpoint, 0, 4)
+
+        drone.step_speed(setpoint, 0, 7)
+        drone.step_angle()
         drone.step()
-        
+
         if np.linalg.norm(drone.position, ord=2) > 100:
             drone.reset()
-    
+
         p.stepSimulation(physicsClientId=client)
         #time.sleep(1/240)
-    
+
     plt.plot(timestep, setpoints)
     plt.plot(timestep, real_data)
     #plt.ylim(-0.3,1)
@@ -229,27 +358,28 @@ def eval_pid_speed_ver(client):
             if drone.position[2] < 3:
                 state = 5
                 vz = 0
-                vx = 12
+                vx = 5
                 start_hover = current_time
         elif state == 5:
             if current_time - start_hover > 6:
                 break
-    
+
         setpoints.append(vz)
         real_data.append(drone.lateral_speed[2])
         timestep.append(current_time)
         current_time += 1/240
-        
+
         drone.step_speed(vx, 0, vz)
+        drone.step_angle()
         drone.step()
-        
+
         if np.linalg.norm(drone.position, ord=2) > 100:
             drone.reset()
-    
+
         p.resetDebugVisualizerCamera(5, 0, -35, drone.position, client)
         p.stepSimulation(physicsClientId=client)
-        time.sleep(1/240)
-    
+        #time.sleep(1/240)
+
     plt.plot(timestep, setpoints)
     plt.plot(timestep, real_data)
     #plt.ylim(-0.3,1)
@@ -258,7 +388,7 @@ def eval_pid_speed_ver(client):
 
 def eval_pid_pos_ver():
     pif_h = PID(3, 0.24, 0.625)
-    
+
     setpoints = []
     out = []
     real_data = []
@@ -277,23 +407,24 @@ def eval_pid_pos_ver():
             setpoint = 0
         elif current_time > 15:
             break
-            
+
         setpoints.append(setpoint)
         real_data.append(drone.position[2])
         timestep.append(current_time)
         current_time += 1/240
-        
+
         o = pif_h.step(setpoint, drone.position[2], 1/240)
         out.append(pif_h.integral)#-drone.lateral_speed[2])
         drone.step_speed(0, 0, o)
+        drone.step_angle()
         drone.step()
-        
+
         if np.linalg.norm(drone.position, ord=2) > 15:
             drone.reset()
-    
+
         p.stepSimulation(physicsClientId=client)
         #time.sleep(1/240)
-    
+
     plt.plot(timestep, setpoints)
     plt.plot(timestep, real_data)
     plt.plot(timestep, out)
@@ -309,14 +440,13 @@ def remote_control_nonholonomic(client):
     keyboard.add_hotkey('ctrl', keyboard_command, args=('reset', 0))
     keyboard.add_hotkey(',', keyboard_command, args=('yaw', 0.1))
     keyboard.add_hotkey('.', keyboard_command, args=('yaw', -0.1))
-    threading.Thread(target=lambda : keyboard.wait(hotkey="alt+f4"), args=()).start()
 
     pid_h = PID(3, 0.24, 0.625)
-    desired_height = 2
     while True:
         print(current_control, drone.lateral_speed)
-        drone.step_speed(current_control['v'], 0, pid_h.step(desired_height, drone.position[2], 1/240))
-        drone.set_yaw(current_control['yaw'])
+        drone.step_speed(current_control['v'], 0, pid_h.step(current_control['height'], drone.position[2], 1/240))
+        drone.set_yaw_rate(current_control['yaw'])
+        drone.step_angle()
         drone.step()
         #if np.linalg.norm(drone.position, ord=2) > 5:
         #    drone.reset()
@@ -324,9 +454,44 @@ def remote_control_nonholonomic(client):
         p.stepSimulation(physicsClientId=client)
         time.sleep(1/240)
 
+def test_pid_speed_hor(client):
+    pid_h = PID(3, 0.24, 0.625)
+    desired_height = 2
+    setpoint = 0
+    setpoints = []
+    real_data = []
+    timestep = []
+    current_time = 0
+    last_time = 0
+    keyboard.add_hotkey(',', keyboard_command, args=('yaw', 0.1))
+    keyboard.add_hotkey('.', keyboard_command, args=('yaw', -0.1))
+    while True:
+        setpoints.append(setpoint)
+        real_data.append(drone.lateral_speed[0])
+        timestep.append(current_time)
+        current_time += 1/240
+
+        drone.step_speed(np.random.uniform(-5, 5), 0, pid_h.step(desired_height, drone.position[2], 1/240))
+        drone.set_yaw_rate(current_control['yaw'])
+        drone.step_angle()
+        drone.step()
+        print(drone.pid_vx.integral)
+
+        if np.linalg.norm(drone.position, ord=2) > 100:
+            drone.reset()
+
+        p.stepSimulation(physicsClientId=client)
+        time.sleep(1/240)
+
+    #plt.plot(timestep, setpoints)
+    #plt.plot(timestep, real_data)
+    #plt.ylim(-0.3,1)
+    #plt.grid()
+    #plt.show()
+
 if __name__ == '__main__':
     client = p.connect(p.GUI)
-    p.setGravity(0, 0, -10, physicsClientId=client) 
+    p.setGravity(0, 0, -10, physicsClientId=client)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     plane_id = p.loadURDF("plane.urdf", physicsClientId=client)
     drone = Quadcopter(client)
@@ -335,7 +500,12 @@ if __name__ == '__main__':
     #eval_pid_speed_ver(client)
     #eval_pid_speed_hor(client)
     #remote_control()
-    remote_control_nonholonomic(client)
+    #test_pid_roll()
+    #test_pid_speed_hor(client)
+    #remote_control_nonholonomic(client)
+    test_pid_speed_hor(client)
     #eval_pid_pos_ver()
     #eval_pid_yaw()
     #eval_pid_roll()
+    #eval_pid_yaw(client)
+    #remote_control(client)
